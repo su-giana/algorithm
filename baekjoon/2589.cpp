c63f12159eb5c5f9d0b15833df08223d337141fd
SGNAN <giananews@gmail.com>
2023-04-07 03:17:21 +0900

    baekjoon 2589 time limit exceeded

--- baekjoon/2589.cpp
+++ baekjoon/2589.cpp
@@ -1,26 +1,49 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 
 using namespace std;
 
 int n, m;
+vector<int> dx = {0, 0, -1, 1};
+vector<int> dy = {-1, 1, 0, 0};
 
-int dfs(vector<vector<bool> > map, int i, int j, int count)
+int getMax(vector<vector<int> >& map, vector<vector<bool> >& visited, int i, int j)
 {
-    if(n == 0 || m == 0) return count-1;
-    if(i>=n || i<0 || j>=m || j<0)    return count-1;    
-    if(map[i][j])                     return count-1;
+    int n = map.size(), m = map[0].size();
+    if((i<0 || i>=n || j<0 || j>=m) || map[i][j] == -1)    return -1;
+    if(visited[i][j])   return map[i][j];
+
+    int temp = map[i][j];
+    visited[i][j] = true;
+    for(int k = 0 ; k<4 ; k++)
+    {
+        int ti = i+dx[k], tj = j + dy[k];
+        temp = max(temp, getMax(map, visited, ti, tj));
+    }
     
-    
-    map[i][j] = true;
-    return max(max(dfs(map, i+1, j, count+1), dfs(map, i-1, j, count+1)), max(dfs(map, i, j-1, count+1), dfs(map, i, j+1, count+1)));
+    return temp;
+}
+
+void dfs(vector<vector<int> >& map, int i, int j, int count)
+{
+    int n = map.size(), m = map[0].size();
+    if((i<0 || i>=n || j<0 || j>=m) || map[i][j] <= count)    return;
+
+    map[i][j] = count;
+
+    for(int k = 0 ; k<4 ; k++)
+    {
+        int ti = i+dx[k], tj = j + dy[k];
+        dfs(map, ti, tj, count+1);
+    }
 }
 
 int main()
 {
     cin>>n>>m;
-    vector<vector<bool> > map(n, vector<bool>(m, false));
+    vector<vector<int> > map(n, vector<int>(m, -1));
 
     for(int i = 0 ; i<n ; i++)
     {
@@ -28,23 +51,28 @@ int main()
         cin>>s;
         for(int j = 0 ; j<m ; j++)
         {
-            if(s[j] == 'W')
-                map[i][j] = true;
-            else
-                map[i][j] = false;
+            if(s[j] == 'L')
+                map[i][j] = INT_MAX;
         }
     }
 
-    int max_el = 0;
-    
+    int answer = 0;
+    vector<vector<int> > copied(n, vector<int>(m));
     for(int i = 0 ; i<n ; i++)
     {
         for(int j = 0 ; j<m ; j++)
         {
             
-            max_el = max(max_el, dfs(map, i, j, 0));
+            if(map[i][j] != -1)
+            {
+                copy(map.begin(), map.end(), copied.begin());
+                dfs(copied, i, j, 1);
+            }
+            
+            vector<vector<bool> > visited(n, vector<bool>(m, false));
+            answer = max(answer, getMax(copied, visited, i, j)-1);
         }
     } 
 
-    cout<<max_el;
+    cout<<answer;
 }
