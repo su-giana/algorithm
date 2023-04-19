1d9c1d06a05ccbbe301c21f593f596bc1d95fc26
SGNAN <giananews@gmail.com>
2023-04-19 21:20:50 +0900

    daily leetcode

--- baekjoon/2565.cpp
+++ baekjoon/2565.cpp
@@ -6,90 +6,66 @@
 
 using namespace std;
 
-bool compare(pair<int, int>& a, pair<int, int>& b)
+bool compare(pair<int, int> a, pair<int, int> b)
 {
     return a.first<b.first;
 }
 
+bool compare2(pair<int, int> a, pair<int, int> b)
+{
+    return a.second<b.second;
+}
+
+
 int main()
 {
     int m;
+    vector<pair<int, int> > lines;
+    set<int> visited;
+    set<pair<int, int> > cables;
     cin>>m;
-    set<pair<int, int> > lines;
-    vector<pair<int, int> > cables;
     for(int i = 0 ; i<m ; i++)
     {
         int a, b;
         cin>>a>>b;
 
-        lines.insert(make_pair(a, b));
+        cables.insert(make_pair(a,b));
     }
 
-    for(auto iter = lines.begin() ; iter != lines.end() ; iter++)
+    for(set<pair<int, int> >::iterator iter = cables.begin() ; iter != cables.end() ; iter++)
     {
-        cables.push_back(*iter);
+        lines.push_back(*iter);
     }
-    sort(cables.begin(), cables.end(), compare);
 
-    int n = cables.size();
-    int overlapped = m - n;
-    vector<vector<int> > dp(n+1, vector<int>(3, 0));
-    for(int i = 0 ; i<n ; i++)
-    {
-        dp[i][0] = cables[i].second;
-        dp[i][1] = cables[i].first;
-    }
-    
+    sort(lines.begin(), lines.end(), compare);
+
     int biggest = INT_MAX;
+    int n = lines.size();
+    vector<int> dp(n+1, 0);
     for(int i = n-1 ; i>=0 ; i--)
     {
-        int cable = dp[i][0];
-        int denominator = dp[i][1];
-        if(cable>=biggest)  {dp[i][2] = dp[i+1][2]; continue;}
-        int consume = 0;
+        pair<int, int> line = lines[i];
+        if(visited.find(line.first) != visited.end())  { dp[i] = dp[i+1]+1; continue; }
+        if( line.second >= biggest) {dp[i] = dp[i+1]; continue;}
+        
+        int selected = 0;
         for(int j = 0 ; j<i ; j++)
         {
-            if(dp[j][0]>cable && dp[j][0]<biggest)
-            {
-                consume++;
-            }
+            selected += (lines[j].second >= line.second && lines[j].second < biggest);
         }
-        if(dp[i][1] == dp[i+1][1] && dp[i+1][0] != -1)  
-        { 
-            biggest = dp[i][0];
-            dp[i][0] = -1;
-            continue;
-        }
-        else if(dp[i][1]==dp[i+1][1] && dp[i+1][0] == -1)
-        {
-            dp[i][0] = -1;
-            dp[i][1] = dp[i+1][1] + 1;
-            continue;
-        }
+        int discarded = i - (max_element(lines.begin(), lines.begin()+i, compare2) - lines.begin());
 
-        int maxIndex = max_element(dp.begin(), dp.begin() + i) - dp.begin(), loss=0;
-        for(int j = maxIndex+1 ; j<=i ; j++)   
+        if(selected>discarded && selected)
         {
-            if(dp[j]<dp[maxIndex])  loss++;
+            dp[i] = dp[i+1] + 1;
         }
-
-        // add this cable
-        if(consume<=loss)
-        {
-            biggest = dp[i][0];
-            dp[i][2] = dp[i+1][2]+consume;   
-        }
-        // left this cable
         else
         {
-            dp[i][0] = 0;
-            dp[i][2] = dp[i+1][2] + 1;
+            dp[i] = dp[i+1] + selected;
+            visited.insert(line.first);
+            biggest = line.second;
         }
-
-        for(int j = 0 ;j<dp.size() ; j++)
-            cout<<dp[j][0]<<" "<<dp[j][1]<<" "<<dp[j][2]<<endl;
-        cout<<endl;
     }
 
-    cout<<dp[0][2]+overlapped;
+    cout<<dp[0] + m - n;
 }
