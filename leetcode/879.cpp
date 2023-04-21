0903e623953fb0528f9c31b09aaa1302821c423c
SGNAN <giananews@gmail.com>
2023-04-21 22:12:04 +0900

    leet daily solve

--- leetcode/879.cpp
+++ leetcode/879.cpp
@@ -51,3 +51,35 @@ public:
         return ans;
     }
 };
+
+class Solution2 {
+public:
+  int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) 
+  {
+    int m = group.size();
+        
+    vector<vector<int> > dp(n+1, vector<int>(minProfit+1, 0));
+    dp[0][0] = 1;
+
+    for(int k = 1 ; k<group.size()+1 ; k++)
+    {
+      int gr = group[k-1];
+      int pr = profit[k-1];
+
+      for(int i = n ; i>=gr ; i--)
+      {
+        for(int j = minProfit ; j>=0 ; j--)
+        {
+          dp[i][j] = (dp[i][j] + dp[i-gr][max(0, j-pr)]) % (1000000007);
+        }
+      }
+    }
+
+    int ans = 0;
+    for(int i = 0 ; i<=n ; i++)
+    {
+      ans = ( ans + dp[i][minProfit] ) % 1000000007;
+    }
+    return ans;
+  }
+};
