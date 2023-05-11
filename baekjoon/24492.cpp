7962f16168a62bebc26d3ec8af8ca94f720416d4
SGNAN <giananews@gmail.com>
2023-05-11 20:03:18 +0900

    24492 prep.

---
+++ baekjoon/24492.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> nums;
+    for(int i = 0 ; i<n ; i++)
+    {
+        int num;
+        cin>>num;
+        nums.push_back(num);
+    }
+
+    
+}
