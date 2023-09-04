8f023f5bef88b0f9888cf03c90de28b7c9465cf6
su-giana <giananews@gmail.com>
2023-09-04 17:33:36 +0900

    Sep. 4 : (algorithm) topology sort

--- baekjoon/1766.cpp
+++ baekjoon/1766.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 using namespace std;
 
 int n, m;
 vector<vector<int> > graph(32000, vector<int>());
 vector<bool> visited(32000, false);
+vector<int> root(320000, 0);
+vector<vector<int> > ans;
+
+bool cmp(vector<int>& a, vector<int>& b)
+{
+    return a[0] < b[0];
+}
 
 void init()
 {
@@ -15,23 +23,62 @@ void init()
     cout.tie(0);
 }
 
-void dfs(int cur)
+void topology_sort(int now)
 {
-    int v_size = graph[cur].size();
+    vector<int> tmp;
+    priority_queue<int, vector<int>, greater<int> > q;
+    visited[now] = true;
+    q.push(now);
 
-    sort(graph[cur].begin(), graph[cur].end());
-
-    for(int i = 0 ; i<v_size ; i++)
+    while(!q.empty())
     {
-        int next = graph[cur][i];
-        if(!visited[next])
+        int cur = q.top();
+        q.pop();
+
+        tmp.push_back(cur);
+
+        int v_size = graph[cur].size();
+        sort(graph[cur].begin(), graph[cur].end());
+        for(int i = 0 ; i<v_size ; i++)
         {
-            visited[next] = true;
-            dfs(next);
+            int next = graph[cur][i];
+            if(!visited[next])
+            {
+                root[next]--;
+                if(root[next] == 0)
+                {
+                    q.push(next);
+                    visited[next] = true;
+                }
+            }
+        }
+        graph[cur].clear();
+    }
+
+    ans.push_back(tmp);
+}
+
+void solve_()
+{
+    for(int i = 0 ; i<n ; i++)
+    {
+        if(!visited[i] && root[i] == 0)
+        {
+            visited[i] = true;
+            topology_sort(i);
         }
     }
 
-    cout<<cur+1<<" ";
+    sort(ans.begin(), ans.end(), cmp);
+    int a_size = ans.size();
+    for(int i = 0 ; i<a_size ; i++)
+    {
+        int s_size = ans[i].size();
+        for(int j = 0 ; j<s_size ; j++)
+        {
+            cout<<ans[i][j]+1<<" ";
+        }
+    }
 }
 
 int main()
@@ -45,15 +92,10 @@ int main()
         int a, b;
         cin>>a>>b;
 
-        graph[b-1].push_back(a-1);
+        root[b-1]++;
+
+        graph[a-1].push_back(b-1);
     }
 
-    for(int i = 0 ; i<n ; i++)
-    {
-        if(!visited[i])
-        {
-            visited[i] = true;
-            dfs(i);
-        }
-    }
+    solve_();
 }
