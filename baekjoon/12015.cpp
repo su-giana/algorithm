0a0dbe6cdac982d430c3e4b867ed5ba02e705ed5
su-giana <giananews@gmail.com>
2023-10-10 14:00:41 +0900

    10 Oct. (algorithm) : binary search

--- baekjoon/12015.cpp
+++ baekjoon/12015.cpp
@@ -5,12 +5,29 @@ using namespace std;
 
 int n;
 vector<int> nums(1000000, 0);
+vector<int> lis;
 
 void init()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+}
+
+int binarySearch(int now)
+{
+    int left = 0;
+    int right = lis.size() - 1;
+
+    while(left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if(lis[mid] >= now)  right = mid;
+        else    left = mid+1;
+    }
+
+    return right;
 }
 
 int main()
@@ -24,5 +41,20 @@ int main()
         cin>>nums[i];
     }
 
-    
+    lis.push_back(nums[0]);
+
+    for(int i = 1 ; i<n ; i++)
+    {
+        if(nums[i] > lis.back())
+        {
+            lis.push_back(nums[i]);
+        }
+        else if(nums[i] < lis.back())
+        {
+            int idx = binarySearch(nums[i]);
+            lis[idx] = nums[i];
+        }
+    }
+
+    cout<<lis.size();
 }
