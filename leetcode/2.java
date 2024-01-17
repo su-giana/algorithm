8b524d3555cc0e1debae45518f97572d36b37672
su-giana <giananews@gmail.com>
2024-01-17 17:17:35 +0900

    2: Add Two Numbers (Linked List)

--- leetcode/2.java
+++ leetcode/2.java
@@ -10,51 +10,34 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if (l1 == null && l2==null) {
-            return null;
-        }
-        else if (l1 == null && l2 != null) {
-            if (l2.val > 9) {
-                if(l2.next != null) {
-                   l2.next.val += 1;
-                }
-                else {
-                    l2.next = new ListNode(1);
-                }
-                l2.val %= 10;
-            }
+        if (l1 == null && l2 == null)   return null;
 
-            return new ListNode(l2.val, addTwoNumbers(null, l2.next));
-        }
-        else if (l1 != null && l2 == null) {
-            if (l1.val > 9) {
-               if (l1.next != null) {
-                    l1.next.val += 1;
-                }
-                else {
-                    l1.next = new ListNode(1);
-                }
-                l1.val %= 10;
-            }
-            
-            return new ListNode(l1.val, addTwoNumbers(l1.next, null));
-        }
-
-        int cur = l1.val + l2.val;
+        int cur = 0;
+        if (l1 != null) cur += l1.val;
+        if (l2 != null) cur += l2.val;
 
         if (cur / 10 > 0) {
-            if (l1.next != null) {
-                l1.next.val += 1;
+            if (l1 != null) {
+                if (l1.next == null) {
+                   l1.next = new ListNode(cur / 10);
+                }
+                else {
+                    l1.next.val += cur / 10;
+                }
             }
-            else if(l2.next != null) {
-                l2.next.val += 1;
-            }
-            else {
-                l1.next = new ListNode(1);
+            else if (l2 != null) {
+                if (l2.next == null) {
+                   l2.next = new ListNode(cur / 10);
+                }
+                else {
+                    l2.next.val += cur / 10;
+                }
             }
         }
 
-        ListNode node = new ListNode(cur % 10, addTwoNumbers(l1.next, l2.next));
-        return node;
+        if (l1 != null) l1 = l1.next;
+        if (l2 != null) l2 = l2.next;
+
+        return new ListNode(cur % 10, addTwoNumbers(l1, l2));
     }
 }
